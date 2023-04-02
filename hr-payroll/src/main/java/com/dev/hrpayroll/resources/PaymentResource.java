eec35b17c1d0c439e3e9d918a1560d480b65d034
Flavio <suporte.flavio.souza@hotmail.com>
2023-04-02 12:53:19 -0300

    Configuração Spring Retry em vez de Hystrix que está depreciado

--- hr-payroll/src/main/java/com/dev/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/dev/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,8 @@ package com.dev.hrpayroll.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +19,16 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService service;
 	
+	@Retryable(maxAttempts = 3)
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
 		Payment payment = service.getPayment(workerId, days);
+		return ResponseEntity.ok(payment);
+	}
+	
+	@Recover
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days){
+		Payment payment = new Payment("Brann", 400.0, days);
 		return ResponseEntity.ok(payment);
 	}
 
