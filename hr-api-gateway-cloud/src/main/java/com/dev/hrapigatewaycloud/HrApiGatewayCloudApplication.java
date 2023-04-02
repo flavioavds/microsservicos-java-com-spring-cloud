9d9986f32d7e6c3f9bf9014d9e102f5ab89a43ef
Flavio <suporte.flavio.souza@hotmail.com>
2023-04-02 13:30:51 -0300

    Project hr-api-gateway-cloud created

---
+++ hr-api-gateway-cloud/src/main/java/com/dev/hrapigatewaycloud/HrApiGatewayCloudApplication.java
@@ -0,0 +1,13 @@
+package com.dev.hrapigatewaycloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGatewayCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatewayCloudApplication.class, args);
+	}
+
+}
